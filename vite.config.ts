c2702edc2ee304ed445a449cc6330776d0fb4c91
unknown <cristianosantos0485@gmail.com>
2026-01-19 20:32:26 -0300

    Disable PWA to fix build

--- vite.config.ts
+++ vite.config.ts
@@ -6,38 +6,38 @@ import { VitePWA } from 'vite-plugin-pwa'
 export default defineConfig({
   plugins: [
     react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
-      manifest: {
-        name: 'Controle de Gastos',
-        short_name: 'Gastos',
-        description: 'Gerenciador de despesas pessoal premium',
-        theme_color: '#00d09c',
-        background_color: '#000000',
-        display: 'standalone',
-        icons: [
-          {
-            src: 'pwa-192x192.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png'
-          },
-          {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any maskable'
-          }
-        ]
-      },
-      devOptions: {
-        enabled: true
-      }
-    })
+    // VitePWA({
+    //   registerType: 'autoUpdate',
+    //   includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+    //   manifest: {
+    //     name: 'Controle de Gastos',
+    //     short_name: 'Gastos',
+    //     description: 'Gerenciador de despesas pessoal premium',
+    //     theme_color: '#00d09c',
+    //     background_color: '#000000',
+    //     display: 'standalone',
+    //     icons: [
+    //       {
+    //         src: 'pwa-192x192.png',
+    //         sizes: '192x192',
+    //         type: 'image/png'
+    //       },
+    //       {
+    //         src: 'pwa-512x512.png',
+    //         sizes: '512x512',
+    //         type: 'image/png'
+    //       },
+    //       {
+    //         src: 'pwa-512x512.png',
+    //         sizes: '512x512',
+    //         type: 'image/png',
+    //         purpose: 'any maskable'
+    //       }
+    //     ]
+    //   },
+    //   devOptions: {
+    //     enabled: true
+    //   }
+    // })
   ],
 })
